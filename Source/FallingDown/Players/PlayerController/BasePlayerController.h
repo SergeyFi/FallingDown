3113a1f5341c182c466caec2073e82f135b61347
Sergey Firsov <zlofks@gmail.com>
2020-01-29 20:35:41 +0300

    Add BasePlayerController

---
+++ Source/FallingDown/Players/PlayerController/BasePlayerController.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+
+UCLASS()
+class FALLINGDOWN_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
